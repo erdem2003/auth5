d539e01c2fd2c211b2af0a4b6b1437d3ac13bb5d
erdem2003 <erdemk2330@gmail.com>
2025-09-22 20:01:46 +0300

    users alanı eklendi

--- index.js
+++ index.js
@@ -108,13 +108,31 @@ app.post("/login", async (req, res) => {
 // ----------------------------
 app.post("/refresh", (req, res) => { //buraya refresh tokenı gönderiyorsun
     const { token } = req.body; //json şeklinde refresh tokenı alıyorum burda
-    if (!token) return res.sendStatus(401);
+    if (!token) return res.sendStatus(401); //burda hata alırsak refresh tokenın süresi geçmiştir.logine atmam lazım kullanıcıyı
 
     jwt.verify(token, REFRESH_SECRET, (err, user) => {
         if (err) return res.sendStatus(403);
         const accessToken = jwt.sign({ id: user.id, email: user.email }, ACCESS_SECRET, { expiresIn: "15m" });
         res.json({ accessToken, refreshToken: token });
     });
+});
+
+// Kullanıcıları listele (kendin hariç)
+// ----------------------------
+app.get("/users", authenticate, async (req, res) => {
+    try {
+        const currentUserId = req.user.id;
+
+        const result = await pool.query(
+            "SELECT id, email FROM users WHERE id != $1",
+            [currentUserId]
+        );
+
+        res.json(result.rows); // JSON array (liste) döner
+    } catch (err) {
+        console.error("Kullanıcıları getirirken hata:", err);
+        res.status(500).json({ error: "Sunucu hatası" });
+    }
 });
 
 // ----------------------------
@@ -125,7 +143,7 @@ function authenticate(req, res, next) {
     const token = authHeader && authHeader.split(" ")[1];
     if (!token) return res.sendStatus(401);
 
-    jwt.verify(token, ACCESS_SECRET, (err, user) => {
+    jwt.verify(token, ACCESS_SECRET, (err, user) => { //burda access tokeni veriyorum aslında.user dedigide accesstoken içindeki payload
         if (err) return res.sendStatus(401);
         req.user = user;  //req.user jwt kimlik dogrulaması eğer başarılıysa token içindeki payload bilgisini taşıyan yapı payload aslında json verisi taşıyor
         //request user jsonunu ekliyor aslında burda.
